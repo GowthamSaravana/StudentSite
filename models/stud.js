7cdb9c93fa781a395d8b7d8e84db7d7df75f1d37
Gowtham S <35022651+GowthamSaravana@users.noreply.github.com>
2020-08-16 15:02:13 +0530

    Update stud.js

--- models/stud.js
+++ models/stud.js
@@ -4,19 +4,16 @@ var Schema = mongoose.Schema;
 
 var StudSchema = new Schema(
   {
-    name: {type: String, required: true},
-    //author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    name: {type: String, required: true};
     age: {type: String, required: true},
-    //isbn: {type: String, required: true},
-    //genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
-  }
+    }
 );
 
-// Virtual for book's URL
+// Virtual for student's URL
 StudSchema
 .virtual('url')
 .get(function () {
-  return '/info/stud/' + this._id;
+  return '/info/students/' + this._id;
 });
 
 //Export model
